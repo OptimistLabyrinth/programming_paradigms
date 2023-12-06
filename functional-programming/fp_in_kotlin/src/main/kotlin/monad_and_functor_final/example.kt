e8efa7db281f4e82d13f6770347cb677b4e69a8a
OptimistLabyrinth <keesung0705@gmail.com>
2023-12-06 17:18:14 +0900

    기존에 만들어둔 함수를 호출할 때마다 로그를 남겨야 한다
    계산 결과값과 로그를 추가한 버전을 함께 반환하도록 수정한다

--- functional-programming/fp_in_kotlin/src/main/kotlin/monad_and_functor_final/example.kt
+++ functional-programming/fp_in_kotlin/src/main/kotlin/monad_and_functor_final/example.kt
@@ -1,16 +1,31 @@
 package monad_and_functor_final
 
-private fun square(x: Int): Int {
-    return x * x
+private data class number_with_logs(
+    var result: Int = 0,
+    var logs: List<String> = ArrayList()
+)
+
+private fun wrap_with_logs(x: Int): number_with_logs {
+    return number_with_logs(x)
 }
 
-private fun add_one(x: Int): Int {
-    return x + 1
+private fun square(x: number_with_logs): number_with_logs {
+    return number_with_logs(
+        result = x.result * x.result,
+        logs = x.logs + listOf("Squared ${x.result} to get ${x.result * x.result}")
+    )
+}
+
+private fun add_one(x: number_with_logs): number_with_logs {
+    return number_with_logs(
+        result = x.result + 1,
+        logs = x.logs + listOf("Added 1 to ${x.result} to get ${x.result + 1}")
+    )
 }
 
 fun run_something_funny() {
-    val result01 = add_one(square(2))
-    val result02 = square(add_one(3))
-    println(result01)
-    println(result02)
+    val result01 = add_one(square(wrap_with_logs(2)))
+    val result02 = square(add_one(wrap_with_logs(3)))
+    println(result01.result)
+    println(result02.result)
 }
