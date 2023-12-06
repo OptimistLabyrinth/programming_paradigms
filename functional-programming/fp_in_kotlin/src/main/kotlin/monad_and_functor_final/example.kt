18a2c6fb928c454d1ab81901b9982d1b14747415
OptimistLabyrinth <keesung0705@gmail.com>
2023-12-06 17:28:06 +0900

    코드 내에 중복되는 요소를 고차함수를 입력받는 함수로 분리하기
    run_with_logs 라는 함수를 통해서
    구현하는 로직이 진행되는 파이프라인 내에서는
    항상 모나드 세상(Monad World) 에만 존재하도록 만들 수 있게 됐다
    
    일반 세상과 모나드 세상을 왕복하는 부분
    * 계산하려고 number_with_logs 라는 모나드 객체에서 result 필드에 저장된 일반객체를 꺼내는 부분
    * 새로운 로그메시지 한 줄을 number_with_logs 라는 모나드 객체의 logs 필드에 추가하기 위해서 일반객체를 꺼내는 부분

--- functional-programming/fp_in_kotlin/src/main/kotlin/monad_and_functor_final/example.kt
+++ functional-programming/fp_in_kotlin/src/main/kotlin/monad_and_functor_final/example.kt
@@ -9,60 +9,64 @@ private fun wrap_with_logs(x: Int): number_with_logs {
     return number_with_logs(x)
 }
 
-private fun square(x: number_with_logs): number_with_logs {
+private fun square(x: Int): number_with_logs {
     return number_with_logs(
-        result = x.result * x.result,
-        logs = x.logs + listOf("Squared ${x.result} to get ${x.result * x.result}")
+        result = x * x,
+        logs = listOf("Squared ${x} to get ${x * x}")
     )
 }
 
-private fun add_one(x: number_with_logs): number_with_logs {
+private fun add_one(x: Int): number_with_logs {
     return number_with_logs(
-        result = x.result + 1,
-        logs = x.logs + listOf("Added 1 to ${x.result} to get ${x.result + 1}")
+        result = x + 1,
+        logs = listOf("Added 1 to ${x} to get ${x + 1}")
     )
 }
 
-private fun minus_five(x: number_with_logs): number_with_logs {
+private fun minus_five(x: Int): number_with_logs {
     return number_with_logs(
-        result = x.result - 5,
-        logs = x.logs + listOf("Subtracted 5 from ${x.result} to get ${x.result - 5}")
+        result = x - 5,
+        logs = listOf("Subtracted 5 from ${x} to get ${x - 5}")
     )
 }
 
-private fun multiply_three(x: number_with_logs): number_with_logs {
+private fun multiply_three(x: Int): number_with_logs {
     return number_with_logs(
-        result = x.result * 3,
-        logs = x.logs + listOf("Multiply by 3 to ${x.result} to get ${x.result * 3}")
+        result = x * 3,
+        logs = listOf("Multiply by 3 to ${x} to get ${x * 3}")
     )
 }
 
-private fun add_twelve(x: number_with_logs): number_with_logs {
+private fun add_twelve(x: Int): number_with_logs {
     return number_with_logs(
-        result = x.result + 12,
-        logs = x.logs + listOf("Added 12 to ${x.result} to get ${x.result + 12}")
+        result = x + 12,
+        logs = listOf("Added 12 to ${x} to get ${x + 12}")
+    )
+}
+
+private fun run_with_logs(
+    input: number_with_logs,
+    transform: (_: Int) -> number_with_logs
+): number_with_logs {
+    val new_umber_with_logs = transform(input.result)
+    return number_with_logs(
+        result = new_umber_with_logs.result,
+        logs = input.logs + new_umber_with_logs.logs
     )
 }
 
 fun run_something_funny() {
     val result01 = wrap_with_logs(2)
-        .run { square(this) }
-        .run { add_one(this) }
-        .run { minus_five(this) }
-        .run { multiply_three(this) }
-        .run { add_twelve(this) }
+        .run { run_with_logs(this) { x -> square(x) } }
+        .run { run_with_logs(this) { x -> add_one(x) } }
+        .run { run_with_logs(this) { x -> minus_five(x) } }
+        .run { run_with_logs(this) { x -> multiply_three(x) } }
+        .run { run_with_logs(this) { x -> add_twelve(x) } }
     val result02 = wrap_with_logs(3)
-        .run { add_one(this) }
-        .run { multiply_three(this) }
-        .run { add_twelve(this) }
-        .run { square(this) }
-    println(result01.result)
-    println(result02.result)
-}
-
-fun run_something_funny_before() {
-    val result01 = add_twelve(multiply_three(minus_five(add_one(square(wrap_with_logs(2))))))
-    val result02 = square(add_twelve(multiply_three(add_one(wrap_with_logs(3)))))
+        .run { run_with_logs(this) { x -> add_one(x) } }
+        .run { run_with_logs(this) { x -> multiply_three(x) } }
+        .run { run_with_logs(this) { x -> add_twelve(x) } }
+        .run { run_with_logs(this) { x -> square(x) } }
     println(result01.result)
     println(result02.result)
 }
