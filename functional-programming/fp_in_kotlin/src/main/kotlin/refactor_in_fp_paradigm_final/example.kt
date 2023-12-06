40042b6d4547d56595ed1c771d7a29d7320f578f
OptimistLabyrinth <keesung0705@gmail.com>
2023-12-06 16:34:16 +0900

    계속해서 암시적인 입출력을 명시적인 입출력으로 바꾸고, 불필요한 변수는 삭제하고, 불필요한 중간 함수 호출을 제거하기

--- functional-programming/fp_in_kotlin/src/main/kotlin/refactor_in_fp_paradigm_final/example.kt
+++ functional-programming/fp_in_kotlin/src/main/kotlin/refactor_in_fp_paradigm_final/example.kt
@@ -6,24 +6,19 @@ private data class shop_item(
 )
 
 private var shopping_cart: MutableList<shop_item> = ArrayList()
-private var shopping_cart_price_total = 0
 
 private fun add_item_to_cart(cart: MutableList<shop_item>, name: String, price: Int) {
     shopping_cart = add_item(cart, name, price)
-    calc_cart_price_total()
+    val total = calc_total(shopping_cart)
+    set_cart_price_total_on_ui(total)
+    update_shipping_icons(cart)
+    update_tax_ui(total)
 }
 
 private fun add_item(cart: MutableList<shop_item>, name: String, price: Int): MutableList<shop_item> {
     val new_cart = cart.slice(0..cart.size - 1) as MutableList<shop_item>
     new_cart.add(shop_item(name, price))
     return new_cart
-}
-
-private fun calc_cart_price_total() {
-    shopping_cart_price_total = calc_total(shopping_cart)
-    set_cart_price_total_on_ui()
-    update_shipping_icons()
-    update_tax_ui()
 }
 
 private fun calc_total(cart: MutableList<shop_item>): Int {
@@ -35,28 +30,33 @@ private fun calc_total(cart: MutableList<shop_item>): Int {
     return price_total
 }
 
-private fun set_cart_price_total_on_ui() {
+private fun set_cart_price_total_on_ui(total: Int) {
     // *
 }
 
-private fun update_shipping_icons() {
+private fun update_shipping_icons(cart: MutableList<shop_item>) {
     val buy_buttons = get_buy_button_icons()
     for (i in 0..buy_buttons.size - 1) {
         val button = buy_buttons[i]
         val item = button.item
-        if (is_free_delivery_available(shopping_cart_price_total, item)) {
-            button.show_free_shipping_icon()
-        } else {
-            button.hide_free_shipping_icon()
-        }
+        val new_cart = add_item(cart, item.name, item.price)
+        set_free_shipping_icon(button, is_free_shipping_available(new_cart))
     }
 }
 
-private fun is_free_delivery_available(cart_price_total: Int, item: shop_item): Boolean {
-    return 20 <= item.price + cart_price_total
+private fun set_free_shipping_icon(button: item_button, isVisible: Boolean) {
+    if (isVisible) {
+        button.show_free_shipping_icon()
+    } else {
+        button.hide_free_shipping_icon()
+    }
 }
 
-private class button(val label: String, val item: shop_item) {
+private fun is_free_shipping_available(cart: MutableList<shop_item>): Boolean {
+    return 20 <= calc_total(cart)
+}
+
+private class item_button(val label: String, val item: shop_item) {
     fun show_free_shipping_icon() {
         // *
     }
@@ -66,16 +66,16 @@ private class button(val label: String, val item: shop_item) {
     }
 }
 
-private fun get_buy_button_icons(): MutableList<button> {
+private fun get_buy_button_icons(): MutableList<item_button> {
     return mutableListOf(
-        button("a", shop_item("item 1", 1)),
-        button("b", shop_item("item 2", 2)),
-        button("c", shop_item("item 3", 3))
+        item_button("a", shop_item("item 1", 1)),
+        item_button("b", shop_item("item 2", 2)),
+        item_button("c", shop_item("item 3", 3))
     )
 }
 
-private fun update_tax_ui() {
-    set_tax_dom(calc_tax(shopping_cart_price_total))
+private fun update_tax_ui(total: Int) {
+    set_tax_dom(calc_tax(total))
 }
 
 private fun calc_tax(amount: Int): Double {
