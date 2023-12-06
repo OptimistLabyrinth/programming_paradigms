2a2e532f7eb686c19acbc5bc374f724eab9e0bf4
OptimistLabyrinth <keesung0705@gmail.com>
2023-12-06 16:36:55 +0900

    사용자 목록을 가지고 필요한 정보를 추출하는 기능을 개발한다고 가정해보자
    사용자 정보는 아이디(id), 이름(name), 나이(age) 를 담고 있다
    
    [1] 나이가 30 살 이상인 사용자들의 이름 목록을 확인하기
    [2] 나이가 30 살 미만인 사용자들의 나이 목록을 확인하기
    
    우선 그냥 생각나는대로 구현해보기

--- functional-programming/fp_in_kotlin/src/main/kotlin/Main.kt
+++ functional-programming/fp_in_kotlin/src/main/kotlin/Main.kt
@@ -5,18 +5,12 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
-    // test_higher_order_function()
+    test_higher_order_function()
     // test_monad_and_functor()
 }
 
 fun test_higher_order_function() {
-    higher_order_function_01.run_something_funny()
-    higher_order_function_02.run_something_funny()
-    higher_order_function_03.run_something_funny()
-    higher_order_function_04.run_something_funny()
-    higher_order_function_05.run_something_funny()
-    higher_order_function_06.run_something_funny()
-    higher_order_function_07.run_something_funny()
+    higher_order_function_final.run_something_funny()
 }
 
 fun test_functional_architecture() {
