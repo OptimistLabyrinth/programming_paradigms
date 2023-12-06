58531622820471ccaafb91e1985f63f093160c6a
OptimistLabyrinth <keesung0705@gmail.com>
2023-12-06 17:22:43 +0900

    다른 함수들도 추가해보자
    minus_five: 5 빼기
    multiply_three: 3 곱하기
    add_twelve: 12 더하기
    
    이렇게 새로 만드는 함수에도 기존과 똑같이 계산하면서 동시에 로그를 남겨야 한다

--- functional-programming/fp_in_kotlin/src/main/kotlin/monad_and_functor_final/example.kt
+++ functional-programming/fp_in_kotlin/src/main/kotlin/monad_and_functor_final/example.kt
@@ -23,9 +23,46 @@ private fun add_one(x: number_with_logs): number_with_logs {
     )
 }
 
+private fun minus_five(x: number_with_logs): number_with_logs {
+    return number_with_logs(
+        result = x.result - 5,
+        logs = x.logs + listOf("Subtracted 5 from ${x.result} to get ${x.result - 5}")
+    )
+}
+
+private fun multiply_three(x: number_with_logs): number_with_logs {
+    return number_with_logs(
+        result = x.result * 3,
+        logs = x.logs + listOf("Multiply by 3 to ${x.result} to get ${x.result * 3}")
+    )
+}
+
+private fun add_twelve(x: number_with_logs): number_with_logs {
+    return number_with_logs(
+        result = x.result + 12,
+        logs = x.logs + listOf("Added 12 to ${x.result} to get ${x.result + 12}")
+    )
+}
+
 fun run_something_funny() {
-    val result01 = add_one(square(wrap_with_logs(2)))
-    val result02 = square(add_one(wrap_with_logs(3)))
+    val result01 = wrap_with_logs(2)
+        .run { square(this) }
+        .run { add_one(this) }
+        .run { minus_five(this) }
+        .run { multiply_three(this) }
+        .run { add_twelve(this) }
+    val result02 = wrap_with_logs(3)
+        .run { add_one(this) }
+        .run { multiply_three(this) }
+        .run { add_twelve(this) }
+        .run { square(this) }
+    println(result01.result)
+    println(result02.result)
+}
+
+fun run_something_funny_before() {
+    val result01 = add_twelve(multiply_three(minus_five(add_one(square(wrap_with_logs(2))))))
+    val result02 = square(add_twelve(multiply_three(add_one(wrap_with_logs(3)))))
     println(result01.result)
     println(result02.result)
 }
